460ab419732cfc6432c810cbaba499b6d422f0fa
ninhtheanh <ninhtheanh@ninhtheanh.tma.com.vn>
2014-12-02 17:42:57 +0700

    Change Lien He - google map Lat-Long

--- include/compiled/atcontent.php
+++ include/compiled/atcontent.php
@@ -44,23 +44,23 @@
                 		<div class="detail_add_title" style="padding-left:0px">Bản đồ</div>
 						<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 						<script type="text/javascript">
-                              function initialize() {		 
-                        
-                                 var geocoder;
-                                var mapDiv = document.getElementById('mapcanvas');
-                                var myLatlng = new google.maps.LatLng(<?php echo $partner['longlat']; ?>);
-                                var map = new google.maps.Map(mapDiv, {
-                                  center: myLatlng,
-                                  zoom: 15,
-                                  mapTypeId: google.maps.MapTypeId.ROADMAP,
-                                  scrollwheel: true
-                                });
+                              function initialize() { 
+
+								 var geocoder;
+								var mapDiv = document.getElementById('mapcanvas');
+								var myLatlng = new google.maps.LatLng(10.795080, 106.651658);
+								var map = new google.maps.Map(mapDiv, {
+								  center: myLatlng,
+								  zoom: 15,
+								  mapTypeId: google.maps.MapTypeId.ROADMAP,
+								  scrollwheel: true
+								});
                                 
                                  geocoder = new google.maps.Geocoder();
                                  
                                  var marker = new google.maps.Marker({
                                         position: myLatlng,
-                                        title: '10.78896,106.676899'
+                                        title: '120/58 Trường Chinh, P12. Quận Tân Bình,TPHCM'
                                     });
                                     marker.setMap(map);
                         
@@ -69,10 +69,14 @@
                               google.maps.event.addDomListener(window, 'load', initialize);
                             </script>
                         <div id="mapcanvas" class="mapbody map" style="width:588px; height:300px;border: 1px solid #ccc;"></div>
+						
+						<div><?php echo htmlspecialchars_decode($page['value']); ?></div>
                 <?php	
 					}
 				?>                  
                 </div>
+								
+
                 <div style="float:left; margin-left:40px; margin-top:10px">
                     <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fcheapdeal.HCM&amp;width=737&amp;height=220&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:737px; height:220px;" allowTransparency="true"></iframe> 
                 </div>
